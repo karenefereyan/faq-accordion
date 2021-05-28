54c9afbeb515a2d5ed9a6eb64f6464de1c4e6c25
Efereyan Karen Simisola <thedevkaren@gmail.com>
2021-05-28 16:40:05 +0100

    Testing responsiveness

---
+++ app.js
@@ -0,0 +1,25 @@
+const arr = document.querySelectorAll(".arrow");
+
+const arrLoop = arr.forEach(e => {
+  e.addEventListener("click", showAnswer);
+});
+
+function showAnswer(e) {
+  const mainBox = e.target.parentElement.parentElement;
+  const arrow = e.target;
+
+  const heading = mainBox.querySelector(".question");
+
+  const para = mainBox.querySelector(".answer");
+  if (para.style.display === "block") {
+    para.style.display = "none";
+    heading.style.color = "hsl(240, 6%, 50%)";
+    heading.style.fontWeight="normal";
+    arrow.style.transform = "rotate(0deg)";
+  } else {
+    para.style.display = "block";
+    heading.style.color = "hsl(237, 12%,33%)";
+    heading.style.fontWeight="bold";
+    arrow.style.transform = "rotate(180deg)";
+  }
+}
--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-let icons = document.getElementsByClassName("dropdown");
-let boldText = document.querySelectorAll(".accordion");
-let i;
-let j;
-let a;
+let arrows = document.querySelectorAll(".dropdown");
 
-for(i=0; i<icons.length; i++){
-    icons[i].addEventListener("click", function(){
-        let panelText = this.parentElement.nextElementSibling;
-        panelText.classList.toggle("show");
-    })
+//Loop through all the question items and show the answer
+const arrowloop = arrows.forEach(e => {
+    e.addEventListener("click", displayText);
+});
+
+
+function displayText(e){
+    const listContainer = e.target.parentElement.parentElement;
+    const arrow = e.target;
+
+    
+    console.log("List container is", listContainer);
+    console.log("arrow is", arrow);
+
+    const questionHeading  = listContainer.querySelector(".accordion p");
+    const answer = listContainer.querySelector(".panel");
+
+    console.log("Answer is", answer);
+    console.log("Heading is", questionHeading);
+    
+    if (answer.style.display === "block") {
+        answer.style.display = "none";
+        questionHeading.style.color = "hsl(240, 6%, 50%)";
+        questionHeading.style.fontWeight="normal";
+        arrow.style.transform = "rotate(0deg)";
+      } else {
+        answer.style.display = "block";
+        questionHeading.style.color = "hsl(237, 12%,33%)";
+        questionHeading.style.fontWeight="bold";
+        arrow.style.transform = "rotate(180deg)";
+      }
 }
